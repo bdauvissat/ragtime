e7ec27cae418af5bd0b2c0da1f09141ca7a7534d
Benjamin Dauvissat <benjamin.dauvissat@adelean.com>
2024-04-07 16:12:22 +0200

    Step 4

--- src/main/java/llm/devoxx/services/QuestionService.java
+++ src/main/java/llm/devoxx/services/QuestionService.java
@@ -3,8 +3,6 @@ package llm.devoxx.services;
 import dev.langchain4j.data.embedding.Embedding;
 import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.model.embedding.EmbeddingModel;
-import dev.langchain4j.model.input.Prompt;
-import dev.langchain4j.model.input.PromptTemplate;
 import dev.langchain4j.model.language.LanguageModel;
 import dev.langchain4j.model.output.Response;
 import dev.langchain4j.store.embedding.EmbeddingMatch;
@@ -20,10 +18,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @ApplicationScoped
 public class QuestionService {
@@ -35,6 +30,7 @@ public class QuestionService {
 
     @Inject
     EmbeddingModel embeddingModel;
+
 
     public CompleteAnswer processQuestion(Question question) {
 
@@ -49,7 +45,9 @@ public class QuestionService {
         List<Answer> answers = new ArrayList<>();
 
         StringBuilder rep = new StringBuilder(question.getQuestion());
+
         LOGGER.info("Generating answer for question: \"{}\"", question.getQuestion());
+
         for (var rel : relevant) {
             answers.add(new Answer(rel.embedded().text(), rel.score(), rel.embedded().metadata()));
 
@@ -58,29 +56,11 @@ public class QuestionService {
         }
 
         if (question.isGenerateAnswer()) {
-
-            PromptTemplate template = PromptTemplate.from(
-                    "Answer the following question :\n"
-                        + "\n"
-                        + "Question:\n"
-                        + "{{question}}\n"
-                        + "\n"
-                        + "Base your answer on the following information:\n"
-                        + "{{information}}"
-            );
-
-            String information = relevant.stream().map(rlv -> rlv.embedded().text()).collect(Collectors.joining("\n\n"));
-            Map<String, Object> templateParameters = new HashMap<>();
-            templateParameters.put("question", question.getQuestion());
-            templateParameters.put("information", information);
-            Prompt prompt = template.apply(templateParameters);
-
-            Response<String> generatedAnswer = languageModel.generate(prompt);
+            Response<String> generatedAnswer = languageModel.generate(rep.toString());
             return new CompleteAnswer(generatedAnswer.content(), answers);
         }
 
         return new CompleteAnswer(Constants.EMPTY_STRING, answers);
-
     }
 
 }
