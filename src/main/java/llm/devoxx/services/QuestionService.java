e6e93912cbdae1342392e51926d88d50785c17cb
Benjamin Dauvissat <benjamin.dauvissat@adelean.com>
2024-10-05 18:12:05 +0200

    Volcamp updates

--- src/main/java/llm/devoxx/services/QuestionService.java
+++ src/main/java/llm/devoxx/services/QuestionService.java
@@ -6,6 +6,8 @@ import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.model.language.LanguageModel;
 import dev.langchain4j.model.output.Response;
 import dev.langchain4j.store.embedding.EmbeddingMatch;
+import dev.langchain4j.store.embedding.EmbeddingSearchRequest;
+import dev.langchain4j.store.embedding.EmbeddingSearchResult;
 import dev.langchain4j.store.embedding.EmbeddingStore;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -14,23 +16,20 @@ import llm.devoxx.json.CompleteAnswer;
 import llm.devoxx.json.Question;
 import llm.devoxx.util.Constants;
 import llm.devoxx.util.Tools;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @ApplicationScoped
+@Slf4j
 public class QuestionService {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(QuestionService.class);
 
     @Inject
     Tools tools;
 
     @Inject
     EmbeddingModel embeddingModel;
-
 
     public CompleteAnswer processQuestion(Question question) {
 
@@ -40,13 +39,19 @@ public class QuestionService {
 
         Embedding queryEmbedded = embeddingModel.embed(question.getQuestion()).content();
 
-        List<EmbeddingMatch<TextSegment>> relevant = store.findRelevant(queryEmbedded,3, 0.55);
+        EmbeddingSearchResult<TextSegment> docs = store.search(EmbeddingSearchRequest.builder()
+                .queryEmbedding(queryEmbedded)
+                .build());
+
+        List<EmbeddingMatch<TextSegment>> relevant = docs.matches();
+
+        relevant.sort((o1, o2) -> o2.score().compareTo(o1.score()));
 
         List<Answer> answers = new ArrayList<>();
 
         StringBuilder rep = new StringBuilder(question.getQuestion());
 
-        LOGGER.info("Generating answer for question: \"{}\"", question.getQuestion());
+        log.info("Generating answer for question: \"{}\"", question.getQuestion());
 
         for (var rel : relevant) {
             answers.add(new Answer(rel.embedded().text(), rel.score(), rel.embedded().metadata()));
